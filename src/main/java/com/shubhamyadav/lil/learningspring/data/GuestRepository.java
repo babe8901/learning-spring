939b82792495f42e1e468e804cc704382d12693e
Shubham Yadav <luckyshubham16@gmail.com>
2023-09-16 02:39:58 +0530

    Data Access -> All repositories created

--- src/main/java/com/shubhamyadav/lil/learningspring/data/GuestRepository.java
+++ src/main/java/com/shubhamyadav/lil/learningspring/data/GuestRepository.java
@@ -3,8 +3,6 @@ package com.shubhamyadav.lil.learningspring.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Date;
-
 @Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
+public interface GuestRepository extends JpaRepository<Guest, Long> {
 }
